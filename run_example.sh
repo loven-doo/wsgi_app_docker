de23b334adbc9fd6fb4ebfbc9bece4d9956a7b65
loven-doo <1429305+loven-doo@users.noreply.github.com>
2018-10-14 16:09:07 +0300

    Update run_example.sh

--- run_example.sh
+++ run_example.sh
@@ -1,4 +1,4 @@
 python3 wsgi_app_docker -pp /webapps/examp_user/examp_project -wp /webapps/examp_user/examp_project/examp_project/wsgi.py \
     -rp /webapps/examp_user/examp_project/requirements.txt \
     -pyv 3.6 -sn example.com \
-    -an example_app -bp example_app_compose_dir
+    -an example_app -bp example_app_composedir
